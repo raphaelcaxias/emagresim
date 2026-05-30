0c27ab792baaf5d18b62ffa6bae41342cdd929b9
raphaelcaxias <pires.xx@gmail.com>
2026-05-30 20:11:23 -0300

    Atualizar o __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,1 @@
-from .database import SupabaseDB
-from .gamification import GamificationSystem
-from .psychology import PsychologyEngine
-from .services import UserService
-
-__all__ = ["SupabaseDB", "GamificationSystem", "PsychologyEngine", "UserService"]
+# Pacote de módulos
