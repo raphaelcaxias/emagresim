5705f3a99e69a2b561734567d7058b96044b158a
raphaelcaxias <pires.xx@gmail.com>
2026-06-02 15:59:29 -0300

    Delete core directory

--- core/payments.py
+++
@@ -1,9 +0,0 @@
-class PaymentService:
-    PLANS = {
-        "free": {"name": "Gratuito", "price": 0},
-        "pro": {"name": "Pro", "price": 29.90},
-        "lifetime": {"name": "Vitalício", "price": 497.00}
-    }
-    
-    def create_checkout_link(self, plan_key, user_email):
-        return f"https://sandbox.mercadopago.com.br/checkout/v1/redirect?pref_id=DEMO_{plan_key}"
