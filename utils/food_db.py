72a1f6a78d16ad58ad63d21ef616d303bea89203
raphaelcaxias <pires.xx@gmail.com>
2026-06-02 10:41:00 -0300

    Update food_db.py

--- utils/food_db.py
+++ utils/food_db.py
@@ -1,14 +1,22 @@
 FOOD_DB = {
-    "arroz": {"name": "Arroz Branco", "portion": "1 concha", "cal": 190, "p": 3.8, "c": 42, "g": 0.3},
-    "feijao": {"name": "Feijão Preto", "portion": "1 concha", "cal": 77, "p": 4.5, "c": 14, "g": 0.5},
-    "frango": {"name": "Peito Frango", "portion": "1 filé", "cal": 198, "p": 36, "c": 0, "g": 4.2},
-    "pao": {"name": "Pão Francês", "portion": "1 unid.", "cal": 150, "p": 4.5, "c": 28, "g": 1.8},
-    "banana": {"name": "Banana Prata", "portion": "1 média", "cal": 89, "p": 1.1, "c": 23, "g": 0.3},
-    "ovo": {"name": "Ovo Cozido", "portion": "1 unid.", "cal": 78, "p": 6.3, "c": 0.6, "g": 5.3},
-    "cafe": {"name": "Café c/ Leite", "portion": "1 xícara", "cal": 65, "p": 2.1, "c": 10, "g": 1.8}
+    "arroz": {"name": "Arroz Branco", "portion": "100g", "cal": 130, "p": 2.5, "c": 28, "g": 0.3},
+    "arroz_integral": {"name": "Arroz Integral", "portion": "100g", "cal": 124, "p": 2.6, "c": 26, "g": 1.0},
+    "feijao": {"name": "Feijão Preto", "portion": "100g", "cal": 77, "p": 4.5, "c": 14, "g": 0.5},
+    "frango": {"name": "Frango Grelhado", "portion": "100g", "cal": 165, "p": 31, "c": 0, "g": 3.6},
+    "carne_bovina": {"name": "Carne Bovina Magra", "portion": "100g", "cal": 190, "p": 26, "c": 0, "g": 9.0},
+    "peixe": {"name": "Peixe Branco", "portion": "100g", "cal": 120, "p": 24, "c": 0, "g": 2.5},
+    "ovo": {"name": "Ovo Cozido", "portion": "1 unidade (50g)", "cal": 78, "p": 6.3, "c": 0.6, "g": 5.3},
+    "batata_doce": {"name": "Batata Doce", "portion": "100g", "cal": 125, "p": 1.0, "c": 28, "g": 0.1},
+    "pao": {"name": "Pão Francês", "portion": "1 unidade (50g)", "cal": 135, "p": 4.5, "c": 26, "g": 1.5},
+    "banana": {"name": "Banana Prata", "portion": "1 unidade (80g)", "cal": 72, "p": 0.9, "c": 18, "g": 0.2},
+    "maca": {"name": "Maçã", "portion": "1 unidade (130g)", "cal": 65, "p": 0.3, "c": 17, "g": 0.2},
+    "salada_mista": {"name": "Salada Mista", "portion": "100g", "cal": 25, "p": 1.5, "c": 5, "g": 0.2},
+    "leite": {"name": "Leite Integral", "portion": "200ml", "cal": 120, "p": 6.5, "c": 9, "g": 6.5},
+    "cafe": {"name": "Café sem Açúcar", "portion": "200ml", "cal": 4, "p": 0.2, "c": 0.6, "g": 0.0},
 }
 
 COMBOS = {
-    "pf": {"name": "PF (Prato Feito)", "items": ["arroz", "feijao", "frango"]},
-    "cafe": {"name": "Café da Manhã", "items": ["pao", "cafe", "banana"]}
+    "pf": {"name": "Prato Feito", "items": ["arroz", "feijao", "frango", "salada_mista"]},
+    "cafe_manha": {"name": "Café da Manhã", "items": ["pao", "cafe", "leite"]},
+    "lanche": {"name": "Lanche da Tarde", "items": ["banana", "pao"]},
 }
