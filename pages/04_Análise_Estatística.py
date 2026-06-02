be74f8cafc2e473904f645957235a97fe4c95c47
raphaelcaxias <pires.xx@gmail.com>
2026-06-02 15:59:07 -0300

    Delete pages directory

--- pages/04_Análise_Estatística.py
+++
@@ -1,3 +0,0 @@
-# COMENTE OU APAGUE:
-# from core.services.gamification_advanced import GamificationAdvanced
-# from core.services.reports import ReportGenerator
--- pages/Perfil.py
+++
@@ -1,9 +0,0 @@
-# MUDE DE:
-from core.payment import PaymentService
-
-# PARA:
-class PaymentService:
-    PLANS = {
-        "free": {"name": "Gratuito", "price": 0},
-        "pro": {"name": "Pro", "price": 29.90}
-    }
